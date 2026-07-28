d87f7ccc8d61ffeee3700a78784b3549b0ebdd9c
Mukool1 <workwithmukool@gmail.com>
2026-07-28 14:09:04 +0530

    Backend completed with post comments search and notifications

--- DevConnect/backend/src/models/post.js
+++ DevConnect/backend/src/models/post.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const commentSchema = mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    text: { type: string, required: true },
+    text: { type: String, required: true },
   },
   { timestamps: true },
 );
